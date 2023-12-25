3615ce4ab197e7abc3bfc8dba8090494d1f9222a
Md. Asadul Islam <asadul_islam@outlook.com>
2023-12-25 14:44:18 +0600

    Initial Commit

---
+++ Movie Archive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie Archive
+//
+//  Created by Md. Asadul Islam on 25/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie Archive/Movie_ArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ArchiveApp.swift
+//  Movie Archive
+//
+//  Created by Md. Asadul Islam on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
